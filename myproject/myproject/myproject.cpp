a5213dfc5603b93af3eb9de2d0e2e3b0b2d17ff9
Anastasia Belyaeva <pinkipuyeeeees@gmail.com>
2020-12-28 14:39:45 +0300

    Создан скелет проекта myproject

--- myproject/myproject/myproject.cpp
+++ myproject/myproject/myproject.cpp
@@ -1,11 +1,11 @@
 // myproject.cpp: определяет точку входа для консольного приложения.
 //
-
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
-
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
+
 	return 0;
 }
-
